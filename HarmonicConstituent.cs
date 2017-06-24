9a6728ad41cbfcbcd6c83ba744b9470c507e05cc
orchardmonkey <orchardmonkey@gmail.com>
2017-06-23 18:30:08 -0700

    a couple modules, still need to work on theoretical equilibrium tides

---
+++ HarmonicConstituent.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyde
+{
+  /// <summary>
+  /// A class to store information for each harmonic constituent, to be used for harmonic prediction of tides.
+  /// The prediction is performed as:
+  /// TidalHeight = Sum of Height for all HarmonicConstituents.
+  /// </summary>
+  public class HarmonicConstituent
+  {
+    /// <summary>
+    /// the id of the constitutent, e.g. M2, O1, etc.
+    /// </summary>
+    public string ID { get; set; }
+
+    /// <summary>
+    /// the description of the constituent
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// also called lag.  This value is in degrees, from 0 to 360
+    /// the phase is different, depending upon the tidal locale we are predicting for.
+    /// the phase is an offset from the theroretical equilibrium phase.
+    /// </summary>
+    public double Phase { get; set; }
+
+    /// <summary>
+    /// The phase for a theoretical equilibrium tide that would exist if there were no continents, just oceans.  
+    /// The equilibrium phase for each constituent calculated for the start of the epoch.
+    /// </summary>
+    public double EquilibriumPhase { get; set; }
+
+
+    /// <summary>
+    /// could be meters or feet, depending on if the desired tide prediction is to be in meters or feet.
+    /// the amplitude is different, depending upon the tidal locale we are predicting for
+    /// </summary>
+    public double Amplitude { get; set; }
+
+    /// <summary>
+    /// the speed of the constituent.  This should be in degrees per hour
+    /// These values are always the same for a given constitutent.  For instance:
+    /// S2 (principal solar, semi-diurnal) period is 12 hours, so it is always 30 degrees/hour, because 360/12 = 30 degrees/hour
+    /// M2 (principal lunar, semi-diurnal) period is 12.42 hours, so it is always 28.98 degrees/hour, because 360/12.42 = 28.98 degrees/hour
+    /// </summary>
+    public double Speed { get; set; }
+
+    /// <summary>
+    /// Calculate the height of this constituent.
+    /// </summary>
+    /// <param name="h">the number of hours since the start of the epoch that the equilibrium phases are based on.</param>
+    /// <returns>the height of this harmonic constituent</returns>
+    public double Height (double hours)
+    {
+      return Amplitude * Math.Cos(DegreesToRadians(Speed * hours + EquilibriumPhase - Phase));
+    }
+
+    /// <summary>
+    /// convert degrees to radians
+    /// </summary>
+    /// <param name="degrees">the degrees to convert</param>
+    /// <returns>the radians</returns>
+    public static double DegreesToRadians(double degrees)
+    {
+      return degrees * Math.PI / 180.0;
+    }
+  }
+}
---
+++ Tide.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyde
+{
+  public class Tide
+  {
+    List<HarmonicConstituent> constituents = new List<HarmonicConstituent>();
+    DateTime epochStart = DateTime.Now;
+
+    public Tide()
+    {
+      InitializeConstituentsForOlympia();
+    }
+
+    public void InitializeConstituentsForOlympia()
+    {
+      // Phase is in degrees.  They vary based upon location, but are constant for each location, because they are offset from the equilibrium phase.
+      // Amplitude is in meters here.  They vary based upon location, but are constant for each location, except there is a modification for the moon's 19 year nodal precession.  Usually an amplitude modifier is calculated for each constituent, and is treated as a constant for the entire year.
+      // Constituent speeds are in degrees per hour, and are calculated from the 6 doodson numbers, but are hardcoded here.  they are the same for any location
+      // equilibrium phase is calculated for the epoch start, and is the phase of the constituent for the meridian 0 degrees longitude, GMT, assuming perfect tidal bulge, no continents or shallow shelves.
+      // so, the equilbrium phase for the sun at the meridian, 0:00 am GMT is always 0, because the sun is at nadir.
+      // https://en.wikipedia.org/wiki/Arthur_Thomas_Doodson
+      constituents.Add(new HarmonicConstituent() { ID = "M2", Speed = 28.9841042, Amplitude = 1.464, Phase = 029.9, EquilibriumPhase = 270.21, Description = "Principal lunar, semidiurnal" });
+      constituents.Add(new HarmonicConstituent() { ID = "K1", Speed = 15.0410686, Amplitude = 0.849, Phase = 288.7, EquilibriumPhase = 017.70, Description = "Sun-Moon angle, diurnal" });
+      constituents.Add(new HarmonicConstituent() { ID = "O1", Speed = 13.9430356, Amplitude = 0.463, Phase = 265.1, EquilibriumPhase = 248.94, Description = "Principal lunar declination" });
+      constituents.Add(new HarmonicConstituent() { ID = "S2", Speed = 30.0000000, Amplitude = 0.348, Phase = 062.4, EquilibriumPhase = 000.00, Description = "Principal solar, semidiurnal" });
+      constituents.Add(new HarmonicConstituent() { ID = "N2", Speed = 28.4397295, Amplitude = 0.281, Phase = 004.3, EquilibriumPhase = 016.12, Description = "Principal lunar elliptic, semidiurnal" });
+      constituents.Add(new HarmonicConstituent() { ID = "P1", Speed = 14.9589314, Amplitude = 0.248, Phase = 287.6, EquilibriumPhase = 349.19, Description = "Principal solar declination" });
+      constituents.Add(new HarmonicConstituent() { ID = "M4", Speed = 57.9682084, Amplitude = 0.055, Phase = 291.0, EquilibriumPhase = 180.42, Description = "Shallow water" });
+      constituents.Add(new HarmonicConstituent() { ID = "M6", Speed = 86.9523127, Amplitude = 0.032, Phase = 142.1, EquilibriumPhase = 090.63, Description = "Shallow water" });
+
+      epochStart = new DateTime(2013, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+    }
+
+    /// <summary>
+    /// predict height of Tide by summing the height of the constituents for a given number of hours since epoch start.  
+    /// </summary>
+    /// <param name="hoursSinceEpochStart">hours since the start of the epoch  that our constituent Amplitudes and Phases were calculated for</param>
+    /// <returns>height of tide in feet</returns>
+    private double PredictTideHeight(double hoursSinceEpochStart)
+    {
+      double tideInMeters =  constituents.Sum(c => c.Height(hoursSinceEpochStart));
+      return tideInMeters * 3.28 + 8.5; // convert meters to feet and add mean water level of ~9 (guess)
+
+
+    }
+
+    /// <summary>
+    /// predict height of Tide by summing the height of the constituents for a given number of hours since epoch start.  
+    /// </summary>
+    /// <param name="dateTime">local time that we wish to calculate Olympia tides for</param>
+    /// <returns>height of tide</returns>
+    public double PredictTideHeight(DateTime timeOfCalculation)
+    {
+      double hoursSinceEpochStart = (timeOfCalculation - epochStart).TotalHours;
+      return PredictTideHeight(hoursSinceEpochStart);
+    }
+
+
+  }
+}
