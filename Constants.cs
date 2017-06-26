0bbc0ff1be399e7d73eac1497549a39271c6f9aa
orchardmonkey <orchardmonkey@gmail.com>
2017-06-26 09:56:33 -0700

    Added constants for indexing harmonic constituents

---
+++ Constants.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyde
+{
+  static class Constants
+  {
+    public const int M2 = 0;
+    public const int S2 = 1;
+    public const int N2 = 2;
+    public const int K1 = 3;
+    public const int M4 = 4;
+    public const int O1 = 5;
+    public const int M6 = 6;
+    public const int MK3 = 7;
+    public const int S4 = 8;
+    public const int MN4 = 9;
+    public const int NU2 = 10;
+    public const int S6 = 11;
+    public const int MU2 = 12;
+    public const int _2N2 = 13;
+    public const int OO1 = 14;
+    public const int LAMBDA2 = 15;
+    public const int S1 = 16;
+    public const int M1 = 17;
+    public const int J1 = 18;
+    public const int MM = 19;
+    public const int SSA = 20;
+    public const int SA = 21;
+    public const int MSF = 22;
+    public const int MF = 23;
+    public const int RHO1 = 24;
+    public const int Q1 = 25;
+    public const int T2 = 26;
+    public const int R2 = 27;
+    public const int _2Q1 = 28;
+    public const int P1 = 29;
+    public const int _2SM2 = 30;
+    public const int M3 = 31;
+    public const int L2 = 32;
+    public const int _2MK3 = 33;
+    public const int K2 = 34;
+    public const int M8 = 35;
+    public const int MS4 = 36;
+    public const int ConstitutentArraySize = 37;
+
+
+    /// <summary>
+    /// Rotation of the Earth on its axis, with respect to the Sun, degrees per hour.
+    /// </summary>
+    public const double T = 15.0;
+
+    /// <summary>
+    /// Revolution of the Earth about the Sun, degrees per hour, with respect to fixed stars.
+    /// </summary>
+    public const double h = 0.04106864;
+
+    /// <summary>
+    /// Revolution of the Moon about the Earth, degrees per hour, with respect to fixed stars.
+    /// </summary>
+    public const double s = 0.54901653;
+
+    /// <summary>
+    /// Precession of the Moon's perigee, degrees per hour, with respect to fixed stars.
+    /// </summary>
+    public const double p = 0.00464183;
+
+    /// <summary>
+    /// Precession of the plane of the Moon's orbit, degrees per hour, with respect to fixed stars.
+    /// </summary>
+    public const double N = -0.00220641;
+  }
+}
--- HarmonicConstituent.cs
+++ HarmonicConstituent.cs
@@ -13,6 +13,148 @@ namespace Tyde
   /// </summary>
   public class HarmonicConstituent
   {
+    public HarmonicConstituent()
+    {
+      Amplitude = 0;
+      Description = string.Empty;
+      Phase = 0;
+      EquilibriumPhase = 0;
+      NodeFactor = 0;
+      Speed = 0;
+    }
+
+
+    /// <summary>
+    /// return an array of all harmonic constituents, initialized with descriptions and speeds.
+    /// later we will update phases and amplitudes based on local, and also update
+    /// EquilibriumPhases and Node factors based on the theoretical equilibrium tide calculated for our prediction epoch.
+    /// </summary>
+    /// <returns>array of all harmonic constituents</returns>
+    public static HarmonicConstituent[] GetInitializedConstituents()
+    {
+      HarmonicConstituent[] constituents = new HarmonicConstituent[Constants.ConstitutentArraySize];
+
+      // Speeds are in degrees per hour.  For example, S2 completes a 360 degree cycle in 12 hours. (360/30 = 12)
+      // T + h gives us rotation of earth with respect to fixed stars.  So T + h - s gives us the lunar day.  
+
+      constituents[Constants.M2].Description = "Principal Lunar, Semi-Diurnal (1 cycle = 1/2 rotation of earth with respect to moon)";
+      constituents[Constants.M2].Speed = 2.0 * Constants.T - 2.0 * Constants.s + 2.0 * Constants.h; // 28.9841042;
+
+      constituents[Constants.S2].Description = "Principal Solar, Semi-Diurnal (1 cycle = 1/2 rotation of earth with respect to sun)";
+      constituents[Constants.S2].Speed = 2.0 * Constants.T; // 30.0000000;
+
+      constituents[Constants.N2].Description = "Principal lunar elliptic, semidiurnal (1 cycle = 1/2 rotation of earth with respect to moon's perigee)";
+      constituents[Constants.N2].Speed = 2.0 * Constants.T - 3.0 * Constants.s + 2.0 * Constants.h + Constants.p;   // 28.4397295;
+
+      constituents[Constants.K1].Description = "Combined Sun-Moon angle (1 cycle = full Rotation of earth with respect to fixed stars)";
+      constituents[Constants.K1].Speed = Constants.T + Constants.h; // 15.0410686;
+
+      constituents[Constants.M4].Description = "Shallow water"; 
+      constituents[Constants.M4].Speed = 57.9682084;
+
+      constituents[Constants.O1].Description = "Principal lunar declination";
+      constituents[Constants.O1].Speed = Constants.T - 2.0 * Constants.s + Constants.h; // 13.9430356;
+
+      constituents[Constants.M6].Description = "Shallow water";
+      constituents[Constants.M6].Speed = 86.9523127;
+
+      constituents[Constants.MK3].Description = "";
+      constituents[Constants.MK3].Speed = 0;
+
+      constituents[Constants.S4].Description = "";
+      constituents[Constants.S4].Speed = 0;
+
+      constituents[Constants.MN4].Description = "";
+      constituents[Constants.MN4].Speed = 0;
+
+      constituents[Constants.NU2].Description = "";
+      constituents[Constants.NU2].Speed = 2.0 * Constants.T - 3.0 * Constants.s + 4.0 * Constants.h - Constants.p; // 28.512
+
+      constituents[Constants.S6].Description = "";
+      constituents[Constants.S6].Speed = 0;
+
+      constituents[Constants.MU2].Description = "";
+      constituents[Constants.MU2].Speed = 0;
+
+      constituents[Constants._2N2].Description = "";
+      constituents[Constants._2N2].Speed = 0;
+
+      constituents[Constants.OO1].Description = "";
+      constituents[Constants.OO1].Speed = 0;
+
+      constituents[Constants.LAMBDA2].Description = "";
+      constituents[Constants.LAMBDA2].Speed = 0;
+
+      constituents[Constants.S1].Description = "";
+      constituents[Constants.S1].Speed = 0;
+
+      constituents[Constants.M1].Description = "";
+      constituents[Constants.M1].Speed = 0;
+
+      constituents[Constants.J1].Description = "";
+      constituents[Constants.J1].Speed = 0;
+
+      constituents[Constants.MM].Description = "precession of Moon's perigee with respect to Moon's phase?";
+      constituents[Constants.MM].Speed = Constants.s - Constants.p; // 0.544
+
+      constituents[Constants.SSA].Description = "";
+      constituents[Constants.SSA].Speed = 0;
+
+      constituents[Constants.SA].Description = "Earth's Perihelion";
+      constituents[Constants.SA].Speed = Constants.h;
+
+      constituents[Constants.MSF].Description = "";
+      constituents[Constants.MSF].Speed = 0;
+
+      constituents[Constants.MF].Description = "";
+      constituents[Constants.MF].Speed = 0;
+
+      constituents[Constants.RHO1].Description = "";
+      constituents[Constants.RHO1].Speed = 0;
+
+      constituents[Constants.Q1].Description = "";
+      constituents[Constants.Q1].Speed = 0;
+
+      constituents[Constants.T2].Description = "";
+      constituents[Constants.T2].Speed = 0;
+
+      constituents[Constants.R2].Description = "";
+      constituents[Constants.R2].Speed = 0;
+
+      constituents[Constants._2Q1].Description = "";
+      constituents[Constants._2Q1].Speed = 0;
+
+      constituents[Constants.P1].Description = "Principal solar declination";
+      constituents[Constants.P1].Speed = Constants.T - Constants.h; // 14.9589314;
+
+      constituents[Constants._2SM2].Description = "";
+      constituents[Constants._2SM2].Speed = 0;
+
+      constituents[Constants.M3].Description = "";
+      constituents[Constants.M3].Speed = 0;
+
+      constituents[Constants.L2].Description = "";
+      constituents[Constants.L2].Speed = 2.0 * Constants.T - Constants.s + 2.0 * Constants.h - Constants.p; // 29.528
+
+      constituents[Constants._2MK3].Description = "";
+      constituents[Constants._2MK3].Speed = 0;
+
+      constituents[Constants.K2].Description = "1/2 rotation of earth with respect to fixed stars";
+      constituents[Constants.K2].Speed = 2.0 * Constants.T + 2.0 * Constants.h; // 30.082
+
+      constituents[Constants.M8].Description = "";
+      constituents[Constants.M8].Speed = 0;
+
+      constituents[Constants.MS4].Description = "";
+      constituents[Constants.MS4].Speed = 0;
+
+
+
+
+      return constituents;
+    }
+
+
     /// <summary>
     /// the id of the constitutent, e.g. M2, O1, etc.
     /// </summary>
